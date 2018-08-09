cdb04ec717b977a7d732a3f71074c9127757eb18
timteplych <timteplych@mail.ru>
2018-08-09 21:27:20 +0800

    Initial commit

---
+++ android/src/ru/ttv/cosmicshooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.ttv.cosmicshooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.ttv.cosmicshooter.CosmicShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CosmicShooter(), config);
+	}
+}
---
+++ core/src/ru/ttv/cosmicshooter/CosmicShooter.java
@@ -0,0 +1,33 @@
+package ru.ttv.cosmicshooter;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class CosmicShooter extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("2560x2560CatInSpace.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(255, 255, 255, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, -1000, -1000);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
