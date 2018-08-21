78b109ebdcc4745259da483be0aa1fc5b5421048
timteplych <timteplych@mail.ru>
2018-08-21 17:46:30 +0800

    ship motion added

--- core/src/ru/ttv/cosmicshooter/screen/sprites/Ship.java
+++ core/src/ru/ttv/cosmicshooter/screen/sprites/Ship.java
@@ -2,18 +2,18 @@ package ru.ttv.cosmicshooter.screen.sprites;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 
 import ru.ttv.cosmicshooter.base.Sprite;
 import ru.ttv.cosmicshooter.math.Rect;
-import ru.ttv.cosmicshooter.math.Rnd;
 
 public class Ship extends Sprite {
     private Rect worldBounds;
 
     public Ship(TextureAtlas atlas) {
         super(atlas.findRegion("main_ship"));
-        //super(new TextureRegion(atlas.findRegion("main_ship").getTexture(),190,143));
+        TextureRegion tr = ((TextureRegion) atlas.findRegion("main_ship"));
+        tr.setRegion(916,95,195,287);
+        regions[0] = tr;
         setHeightProportion(0.15f);
     }
 
@@ -26,6 +26,10 @@ public class Ship extends Sprite {
     }
 
     public void changePosition(float x, float y){
+        System.out.println(regions[frame].getRegionWidth());
+        System.out.println(regions[frame].getRegionHeight());
+        System.out.println(pos.x);
+        System.out.println(pos.y);
         pos.set(pos.x + x, pos.y + y);
     }
 }
